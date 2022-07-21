0f5a740e33aa407334126a47ab30debfd60d275e
WynriusCoder479 <wynrius12345678@gmail.com>
2022-07-21 20:09:35 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
 
 const registerRoute = require('./routes/auth/register')
 const loginRoute = require('./routes/auth/login')
@@ -29,9 +28,23 @@ const connrctDB = async () => {
 connrctDB()
 const authPath = '/api/auth'
 const postPath = '/api/posts'
+const accessControlAllow = 'Access-Control-Allow'
 
 const app = express()
-app.use('*', cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use((req, res, next) => {
+	res.setHeader(`${accessControlAllow}-Origin`, process.env.REACT_URL)
+	res.setHeader(
+		`${accessControlAllow}-Methods`,
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	)
+	res.setHeader(
+		`${accessControlAllow}-Headers`,
+		'X-Requested-With, content-type'
+	)
+	res.setHeader(`${accessControlAllow}-Credentials`, true)
+
+	next()
+})
 app.use(express.json())
 
 app.use(authPath, registerRoute)
