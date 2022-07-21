1a8a8be062670340aa37ffa0593a9aeffb74b870
WynriusCoder479 <wynrius12345678@gmail.com>
2022-07-21 19:53:50 +0700

    Removre cors lib

--- client/src/contexts/constancs.js
+++ client/src/contexts/constancs.js
@@ -1,6 +1,6 @@
 export const apiUrl =
 	process.env.NODE_ENV !== 'production'
-		? 'https://vast-plains-77852.herokuapp.com/'
+		? 'https://mern-app-lernit.herokuapp.com/'
 		: 'nothing'
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'learnit-mern-app'
